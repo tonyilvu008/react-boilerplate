9ef53427db3f7a3f9aab2f00a8f06a81e64ec4c1
Tao Zhao <tao.zhao@rakuten.com>
2025-03-24 15:00:55 +0900

    Your commit message

--- public/login/login.js
+++ public/login/login.js
@@ -1,15 +1,15 @@
 // Wait for the DOM to fully load
 document.addEventListener('DOMContentLoaded', function () {
   // Handle WeChat login icon click
-  const wechatLoginIcon = document.getElementById('wechatLoginIcon');
-  if (wechatLoginIcon) {
-    wechatLoginIcon.addEventListener('click', function () {
-      // Redirect to the WeChat login page
-      window.location.href = 'login/wechat-login.html'; // Replace with your actual WeChat login page URL
-    });
-  } else {
-    console.error('WeChat login icon not found in the DOM.');
-  }
+  // const wechatLoginIcon = document.getElementById('wechatLoginIcon');
+  // if (wechatLoginIcon) {
+  //   wechatLoginIcon.addEventListener('click', function () {
+  //     // Redirect to the WeChat login page
+  //     window.location.href = 'login/wechat-login.html'; // Replace with your actual WeChat login page URL
+  //   });
+  // } else {
+  //   console.error('WeChat login icon not found in the DOM.');
+  // }
 
   // Handle Email login icon click
   const emailLoginIcon = document.getElementById('emailLoginIcon');
